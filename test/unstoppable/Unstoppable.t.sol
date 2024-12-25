33bd7bbcff3945ebb11ccb421165ce4ab55ec1b5
Zainab Oufqir <zayna.oufkir@gmail.com>
2024-12-25 23:30:15 +0100

    Challenge 1 Unstoppable done

--- test/unstoppable/Unstoppable.t.sol
+++ test/unstoppable/Unstoppable.t.sol
@@ -91,7 +91,7 @@ contract UnstoppableChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_unstoppable() public checkSolvedByPlayer {
-        
+        token.transfer(address(vault), 1);
     }
 
     /**
